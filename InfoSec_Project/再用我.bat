73bf891c6fb390a71b891ba64151dce7f54e2d95
zxl <zhoudaniel02@gmail.com>
2025-06-03 07:45:14 +0800

    fix ngrok can't work bug

--- InfoSec_Project/再用我.bat
+++ InfoSec_Project/再用我.bat
@@ -1,2 +1,4 @@
-start cmd /k ngrok http 3000
 start cmd /k node server.js
+ngrok config add-authtoken 2vPO73o2WDDzTiCXFOIP0cNm5nI_ho3umWekybh9t1qoAuDp
+start cmd /k ngrok http http://localhost:3000
+
