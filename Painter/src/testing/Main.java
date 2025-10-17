03870203a5985b7c4fbbb5f6fd499a447d2b17c9
zxl <zhoudaniel02@gmail.com>
2025-10-17 16:32:10 +0800

    add the example

--- Painter/src/testing/Main.java
+++ Painter/src/testing/Main.java
@@ -4,8 +4,8 @@ import javafile.Scene;
  * we did it! we can just add the code instead fixed the old code to add the new feature
  */
 public class Main {
-public static void main(String[]args) {
-	Scene scene=new Scene();
-	scene.execute();
-}
+	public static void main(String[]args) {
+		Scene scene=new Scene();
+		scene.execute();
+	}
 }
